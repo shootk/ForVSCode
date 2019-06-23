bfc001fb67da2011fd028edc1a51fea981352821
shootk <shoot.k.uta.7@gmail.com>
2019-06-23 14:16:44 +0900

    ok

---
+++ Study_Python_AI_Programing/python_ai_programing/2/2-4.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn import preprocessing
+
+input_labels = ['red','black','red','green','black','yellow','white']
+
+encoder = preprocessing.LabelEncoder()
+encoder.fit(input_labels)
+
+print("Label mapping")
+for i in enumerate(encoder.clasees_)
+    print(item "-->" i)
+
+test_labels = ['green','yellow','black']
+encoded_values = encoder.transform(test_labels)
+print("Labels = " test_labels)
+print("Encoded values = " encoded_values)
+
+encoded_values = [3,0,4,1]
+decoded_list　= encoder.inverse_transform(encoded_values)
+print("Encoded values = " encoded_values)
+print("Decoded list = "list(Decoded_list))
