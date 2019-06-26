95ce7809c66707d62df2c9e33b96125728324b6e
shootk <shoot.k.uta.7@gmail.com>
2019-06-26 13:01:59 +0900

    ok

--- Study_Python_AI_Programing/python_ai_programing/2/2-4.py
+++ Study_Python_AI_Programing/python_ai_programing/2/2-4.py
@@ -7,15 +7,15 @@ encoder = preprocessing.LabelEncoder()
 encoder.fit(input_labels)
 
 print("Label mapping")
-for i in enumerate(encoder.clasees_)
-    print(item "-->" i)
+for i in enumerate(encoder.clasees_):
+    print(item ,"-->", i)
 
 test_labels = ['green','yellow','black']
 encoded_values = encoder.transform(test_labels)
-print("Labels = " test_labels)
-print("Encoded values = " encoded_values)
+print("Labels = ", test_labels)
+print("Encoded values = ", list(encoded_values))
 
 encoded_values = [3,0,4,1]
-decoded_list　= encoder.inverse_transform(encoded_values)
-print("Encoded values = " encoded_values)
-print("Decoded list = "list(Decoded_list))
+decoded_list = encoder.inverse_transform(encoded_values)
+print("Encoded values = ", encoded_values)
+print("Decoded list = ", list(Decoded_list))
