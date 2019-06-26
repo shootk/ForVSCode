9a4b7eb36d5efe0b44e76bcab11c80c6350293b9
shootk <shoot.k.uta.7@gmail.com>
2019-06-26 14:25:17 +0900

    ok

--- Study_Python_AI_Programing/python_ai_programing/2/2-3.py
+++ Study_Python_AI_Programing/python_ai_programing/2/2-3.py
@@ -14,8 +14,8 @@ print("AFTER")
 print("Mean = ",data_scaled.mean(axis = 0))
 print("Std deviation =",data_scaled.std(axis = 0))
 
-data_scaler_minmax = preprocessing.MinMaxScaler(feature_range(0,1))
-data_scaled_minmax = data_scaler_minmax.fit_transfarm(input_data)
+data_scaler_minmax = preprocessing.MinMaxScaler(feature_range=(0,1))
+data_scaled_minmax = data_scaler_minmax.fit_transform(input_data)
 print("Min max scaled data:\n",data_scaled_minmax)
 
 data_normalized_l1 = preprocessing.normalize(input_data,norm = 'l1')
