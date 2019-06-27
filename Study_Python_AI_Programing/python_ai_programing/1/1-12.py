932a5dffc3b877373b24af9e39ce73f72d680de9
shootk <shoot.k.uta.7@gmail.com>
2019-06-27 13:57:24 +0900

    ok

--- Study_Python_AI_Programing/python_ai_programing/1/1-12.py
+++
@@ -1,5 +0,0 @@
-from sklearn import datasets
-home_prices = datasets.load_boston()
-print(home_prices.target)
-digits = datasets.load_digits()
-print(digits.images[4])
--- Study_Python_AI_Programing/python_ai_programing/2/utilities.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
-
-
-def visualize_classifier(classifier, XmY, title=''):
-    min_x, max_x = X[:, 0].min() - 1.0, X[:, 0].max() + 1.0
-    min_y, max_y = X[:, 1].min() - 1.0, X[:, 1].max() + 1.0
