f4bdb6820962f746697dc285e909bffee3c13a5c
shootk <shoot.k.uta.7@gmail.com>
2019-07-25 19:44:16 +0900

    ok

---
+++ Study_Python_AI_Programming/soccer_test/main.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+import glob
+import numpy as np
+import random
+import math
+
+
+root_dir = '/Users/a1514/OneDrive/デスクトップ/VSCode Making and Studying/Study_Python_AI_Programming/soccer_test'
+
+categories = ["Messi", "Ronaldo", "Neymar", "Mbappe", "Salah", "Suarez",
+              "Hazard", "Griezman", "Ibrahimovic", "Aguero", "Lewandowski"]
+
+X = []
+
+Y = []
