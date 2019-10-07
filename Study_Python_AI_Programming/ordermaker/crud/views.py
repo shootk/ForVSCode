ac9c7edcb3b8011dae0bc4c333ade4f5f2458142
shootk <shoot.k.uta.7@gmail.com>
2019-10-07 11:13:00 +0900

    ok

--- Study_Python_AI_Programming/ordermaker/crud/views.py
+++ Study_Python_AI_Programming/ordermaker/crud/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Message
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'crud/index.html', {})
+    d = {
+        'messages': Message.objects.all(),
+    }
+    return render(request, 'crud/index.html', d)
 
 
 def add(request):
