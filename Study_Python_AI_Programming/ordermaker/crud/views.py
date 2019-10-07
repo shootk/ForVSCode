848c1947e5d7ffa4d00eb57f88e5a222e3a2a3fb
shootk <shoot.k.uta.7@gmail.com>
2019-10-07 22:52:41 +0900

    2019/10/07/22:52

--- Study_Python_AI_Programming/ordermaker/crud/views.py
+++ Study_Python_AI_Programming/ordermaker/crud/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 from .models import Message
+from .forms import MessageForm
 # Create your views here.
 
 
@@ -12,12 +13,36 @@ def index(request):
 
 
 def add(request):
-    return render(request, 'crud/edit.html', {})
+    form = MessageForm(request.POST or None)
+    if form.is_valid():
+        Message.objects.create(**form.cleaned_data)
+        return redirect('crud:index')
+
+    d = {
+        'form': form,
+    }
+    return render(request, 'crud/edit.html', d)
 
 
 def edit(request, editing_id):
-    return render(request, 'crud/edit.html', {})
+    message = get_object_or_404(Message, id=editing_id)
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message.message = form.cleaned_data['message']
+            message.save()
+            return redirect('crud:index')
+    else:
+        form = MessageForm({'message': message.message})
+    d = {
+        'form': form,
+    }
+    return render(request, 'crud/edit.html', d)
 
 
+@require_POST
 def delete(request):
-    return HttpResponse('Delete')
+    delete_ids = request.POST.getlist('delete_ids')
+    if delete_ids:
+        Message.objects.filter(id__in=delete_ids).delete()
+    return redirect('crud:index')
