1b7836dd2fd1c3de14dd9df0b47c93b56af7374d
shootk <shoot.k.uta.7@gmail.com>
2020-02-17 16:24:41 +0900

    ok

---
+++ Litalico/blue_test.py
@@ -0,0 +1,41 @@
+import serial
+import serial.tools.list_ports
+
+
+def uart_write_read(w_data, r_size):
+    # Write
+    ser.write(w_data)
+    print('Send: ' + str(w_data))
+
+    # Read
+    r_data = ser.read_until(size=r_size)  # size分Read
+    print('Recv: ' + str(r_data))
+
+    return r_data
+
+
+def search_com_port():
+    coms = serial.tools.list_ports.comports()
+    comlist = []
+    for com in coms:
+        comlist.append(com.device)
+    print('Connected COM ports: ' + str(comlist))
+    use_port = comlist[1]
+    print('Use COM port: ' + use_port)
+
+    return use_port
+
+
+if __name__ == '__main__':
+    # Search COM Ports
+    use_port = search_com_port()
+
+    # Init Serial Port Setting
+    ser = serial.Serial(use_port)
+    ser.baundrate = 9600
+    ser.timeout = 5  # sec
+
+    w_data = b'RV\n'
+    r_size = 100
+    r_data = uart_write_read(w_data, r_size)
+    print('Reserved: {}'.format(r_data))
